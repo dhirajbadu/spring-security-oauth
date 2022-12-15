6e53c6d7945834f2d31be39238557b9ca66f6bc2
dhiraj <dhirajbadu50@gmail.com>
2022-12-15 18:26:10 +0545

    alternative for validate preferred username

---
+++ oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/CustomAuthenticationManager.java
@@ -0,0 +1,63 @@
+package com.baeldung.resource.spring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.BadJwtException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.server.resource.BearerTokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+@Service
+public class CustomAuthenticationManager implements AuthenticationManager {
+
+    private final Log logger = LogFactory.getLog(this.getClass());
+    private final JwtDecoder jwtDecoder;
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+    public CustomAuthenticationManager(JwtDecoder jwtDecoder) {
+        Assert.notNull(jwtDecoder, "jwtDecoder cannot be null");
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        BearerTokenAuthenticationToken bearer = (BearerTokenAuthenticationToken) authentication;
+        Jwt jwt = this.getJwt(bearer);
+
+        AbstractAuthenticationToken token = (AbstractAuthenticationToken) this.jwtAuthenticationConverter.convert(jwt);
+        if (!jwt.getClaim("preferred_username").toString().endsWith("@test.com")) {
+            throw new AuthenticationServiceException("the token does not contains valid email");
+        }
+        token.setDetails(bearer.getDetails());
+        this.logger.debug("Authenticated token");
+        return token;
+    }
+
+    private Jwt getJwt(BearerTokenAuthenticationToken bearer) {
+        try {
+            return this.jwtDecoder.decode(bearer.getToken());
+        } catch (BadJwtException var3) {
+            this.logger.debug("Failed to authenticate since the JWT was invalid");
+            throw new InvalidBearerTokenException(var3.getMessage(), var3);
+        } catch (JwtException var4) {
+            throw new AuthenticationServiceException(var4.getMessage(), var4);
+        }
+    }
+
+    public boolean supports(Class<?> authentication) {
+        return BearerTokenAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    public void setJwtAuthenticationConverter(Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter) {
+        Assert.notNull(jwtAuthenticationConverter, "jwtAuthenticationConverter cannot be null");
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
+}
--- oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
+++ oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
@@ -1,22 +1,23 @@
 package com.baeldung.resource.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationManager  authenticationManager;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors()
             .and()
+                .authenticationManager(authenticationManager)
             .authorizeRequests()
             .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
             .hasAuthority("SCOPE_read")
@@ -28,22 +29,5 @@ public class SecurityConfig {
             .oauth2ResourceServer()
             .jwt();
         return http.build();
-    }
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.preferredUsername}")
-    private String preferredUsername;
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
-                JwtDecoders.fromOidcIssuerLocation(issuer);
-        OAuth2TokenValidator<Jwt> usernameValidator = new UsernameValidator(preferredUsername);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withUsername = new DelegatingOAuth2TokenValidator<>(withIssuer, usernameValidator);
-        jwtDecoder.setJwtValidator(withUsername);
-        return jwtDecoder;
     }
 }
